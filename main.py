81591fa92d0d05a74c3886b55e7f0b34fee266b9
Shawn Collinge <shawncollinge@gmail.com>
2022-08-23 22:48:06 +0000

    Changed to snoking API

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time, os
+import time, os, requests
 from datetime import datetime
 from selenium import webdriver
-from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -10,7 +9,7 @@ import pandas as pd
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-URL =  "https://snokinghockeyleague.com/#/home/team/1081/2609" # enter team url here for the season
+TEAM_URL =  "https://snokinghockeyleague.com/#/home/team/1081/2609" # enter team url here for the season
 GAME_DURATION = "1:30"
 TYPE = "GAME"
 GAME_TYPE = "REGULAR"
@@ -20,16 +19,16 @@ PASSWORD = ""
 options = Options()
 options.headless = True
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver.get(URL)
-time.sleep(3)
-scheduleLink = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div/div/div/div/div/fieldset/div/div/div[2]/div/div/ul/li[2]/a')
-scheduleLink.click()
-time.sleep(3)
-html = driver.page_source
-MySoup = BeautifulSoup(html, "html.parser")
-results = MySoup.find(name="h4", text="Schedule").find_next(name="table")
-schedule = results.find_all("tr")
-fileName = f"{URL.split('/')[-1]}.csv"
+
+TEAM_URL = TEAM_URL.split("/")
+team = TEAM_URL[-1]
+season = TEAM_URL[-2]
+url = f"https://api.codetabs.com/v1/proxy/?quest=https://snokinghockeyleague.com/api/game/list/{season}/0/{team}"
+
+fileName = f"{team}.csv"
+
+response = requests.get(url)
+schedule = response.json()
 
 gameSchedule = {
                 "Type": {},
@@ -44,24 +43,20 @@ gameSchedule = {
                 }
                 
 today = datetime.now()
-for i in range(len(schedule[1:])):
-    game = schedule[i+1]
-    rawDate = game.find_next("td")
-    date = datetime.strptime(rawDate.getText(), "%m/%d/%Y")
+for i in range(len(schedule)):
+    game = schedule[i]
+    date = datetime.strptime(game['date'], "%m/%d/%Y")
     if date.date() > today.date():
-        gameTime = rawDate.find_next("td").find_next("td")
-        rink = gameTime.find_next("td")
-        home = rink.find_next("td")
-        away = home.find_next("td")
         gameSchedule['Title'][i+1] = ""
         gameSchedule['Type'][i+1] = TYPE
         gameSchedule['Game Type'][i+1] = GAME_TYPE
-        gameSchedule['Home'][i+1] = home.getText()
-        gameSchedule['Away'][i+1] = away.getText()
+        gameSchedule['Home'][i+1] = game['teamHomeName']
+        gameSchedule['Away'][i+1] = game['teamAwayName']
         gameSchedule['Date'][i+1] = date.strftime("%d/%m/%Y")
-        gameSchedule['Time'][i+1] = gameTime.getText()
+        gameSchedule['Time'][i+1] = game['time']
         gameSchedule['Duration'][i+1] = GAME_DURATION
-        gameSchedule['Location'][i+1] = rink.getText()
+        gameSchedule['Location'][i+1] = game['rinkName']
+
 
 df = pd.DataFrame(gameSchedule)
 df.to_csv(fileName, index=False)
